bf3fc8318c43d03a5d273f351671383c6024c8d9
hoochicken <mareike.riegel@ql.de>
2020-10-03 20:19:42 +0200

    Table now with material

--- dungeon-client/src/components/hero/list.vue
+++ dungeon-client/src/components/hero/list.vue
@@ -1,106 +1,110 @@
 <template>
     <div class="container">
         <h1>Heros</h1>
-        <search @resetSearch="resetSearch" @search="search" />
-        <table class="table table-striped table-hover">
-            <thead>
-            <tr>
-                <th>ID</th>
-                <th>Name</th>
-                <th>LE</th>
-                <th>gerade</th>
-                <th></th>
-            </tr>
-            </thead>
-            <tbody>
-            <template v-for="item in heros">
-                <tr v-bind:key="item.id" v-bind:class="(currentId === item.id) ? 'bg-dark text-light' : ''">
-                    <td>{{ item.id }}</td>
-                    <td>{{ item.name}}</td>
-                    <td>{{ item.le }}</td>
-                    <td>{{ item.le_current }}</td>
-                    <td><button class="btn btn-success text-white update" @click="updateHero(item.id)">Update</button><button class="btn btn-danger delete" @click="deleteHero(item.id)">Delete</button></td>
-                </tr>
-            </template>
-            </tbody>
-        </table>
+        <search @resetSearch="resetSearch" @search="search"/>
+        <md-table class="table table-striped table-hover">
+                <md-table-row>
+                    <md-table-head>ID</md-table-head>
+                    <md-table-head>Name</md-table-head>
+                    <md-table-head>LE</md-table-head>
+                    <md-table-head>gerade</md-table-head>
+                    <md-table-head>asd</md-table-head>
+                </md-table-row>
+                <template v-for="item in heros">
+                    <md-table-row v-bind:key="item.id"
+                                  v-bind:class="(currentId === item.id) ? 'bg-dark text-light' : ''">
+                        <md-table-cell>{{ item.id }}</md-table-cell>
+                        <md-table-cell>{{ item.name}}</md-table-cell>
+                        <md-table-cell>{{ item.le }}</md-table-cell>
+                        <md-table-cell>{{ item.le_current }}</md-table-cell>
+                        <md-table-cell>
+                            <button class="btn btn-success text-white update" @click="updateHero(item.id)">Update
+                            </button>
+                            <button class="btn btn-danger delete" @click="deleteHero(item.id)">Delete</button>
+                        </md-table-cell>
+                    </md-table-row>
+                </template>
+        </md-table>
         <pagination :totalPage="listState.totalPage" @btnClick="changePage"></pagination>
         <button class="btn btn-success " @click="$router.push('/hero/create')">Create New Hero</button>
     </div>
 </template>
 
 <script>
-  import Search from "../global/search";
-  export default {
-    name: 'hero-list',
-      components: {Search},
-      data() {
-        return {
-            heros: [],
-            currentId: 0,
-            response: {},
-            searchterm: '',
-            listState: {
-                maxResults: 3,
-                currentPage:0,
-                totalPage: 0,
-                totalItems: 0
-            },
-            listStateDefault: {
-                maxResults: 3,
-                currentPage:0,
-                totalPage: 0,
-                totalItems: 0
+    import Search from "../global/search";
+
+    export default {
+        name: 'hero-list',
+        components: {Search},
+        data() {
+            return {
+                heros: [],
+                currentId: 0,
+                response: {},
+                searchterm: '',
+                listState: {
+                    maxResults: 3,
+                    currentPage: 0,
+                    totalPage: 0,
+                    totalItems: 0
+                },
+                listStateDefault: {
+                    maxResults: 3,
+                    currentPage: 0,
+                    totalPage: 0,
+                    totalItems: 0
+                }
             }
+        },
+        async mounted() {
+            this.list()
+        },
+        methods: {
+            async decrease(id) {
+                this.currentId = id;
+                var params = new URLSearchParams();
+                params.append('id', id);
+                this.response = await this.axios.post('/hero/decrease', params);
+                this.list();
+            },
+            async list() {
+                let params = new URLSearchParams();
+                params.append('searchterm', this.searchterm);
+                params.append('listState', JSON.stringify(this.listState));
+                const response = await this.axios.post('/hero/list', params);
+                this.heros = response.data.items;
+                this.listState = response.data.listState;
+            },
+            async deleteHero(id) {
+                if (!confirm('Really delete this hero???')) {
+                    return;
+                }
+                await this.axios.post('/hero/delete/' + id);
+                this.list();
+            },
+            updateHero(id) {
+                this.$router.push('/hero/update/' + id);
+            },
+            async search(searchterm) {
+                this.searchterm = searchterm;
+                this.listState = this.listStateDefault;
+                this.list();
+            },
+            async resetSearch() {
+                this.searchterm = '';
+                this.listState = this.listStateDefault;
+                this.list();
+            },
+            changePage: function (n) {
+                this.listState.currentPage = n > 0 ? n - 1 : n;
+                this.list();
+            },
         }
-    },
-    async mounted () {
-      this.list()
-    },
-    methods: {
-      async decrease(id) {
-          this.currentId = id;
-          var params = new URLSearchParams();
-          params.append('id', id);
-          this.response = await this.axios.post('/hero/decrease', params);
-          this.list();
-      },
-      async list() {
-          let params = new URLSearchParams();
-          params.append('searchterm', this.searchterm);
-          params.append('listState', JSON.stringify(this.listState));
-          const response = await this.axios.post('/hero/list', params);
-          this.heros = response.data.items;
-          this.listState = response.data.listState;
-      },
-      async deleteHero(id) {
-          if (!confirm('Really delete this hero???')) {
-              return;
-          }
-          await this.axios.post('/hero/delete/' + id);
-          this.list();
-      },
-      updateHero(id) {
-          this.$router.push('/hero/update/' + id);
-      },
-      async search(searchterm) {
-          this.searchterm = searchterm;
-          this.listState = this.listStateDefault;
-          this.list();
-      },
-      async resetSearch() {
-          this.searchterm = '';
-          this.listState = this.listStateDefault;
-          this.list();
-      },
-      changePage : function(n) {
-          this.listState.currentPage = n > 0 ? n - 1 : n;
-          this.list();
-      },
     }
-  }
 </script>
 
 <style scoped>
-    .delete {margin-left:15px;}
+    .delete {
+        margin-left: 15px;
+    }
 </style>
