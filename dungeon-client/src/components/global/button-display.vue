aa39f7de542cb658f9773ff8c5d87c78385f7ea6
hoochicken <mareike.riegel@ql.de>
2020-10-15 22:07:02 +0200

    globals: button for edit, display and update generated

---
+++ dungeon-client/src/components/global/button-display.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <md-button class="md-raised" @click="$router.push(clickRoute)">Display</md-button>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "button-display",
+        props: {
+            clickRoute: {
+                type: String
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ dungeon-client/src/components/global/button-edit.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <md-button class="md-raised" @click="$router.push(clickRoute)">Edit</md-button>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "button-edit",
+        props: {
+            clickRoute: {
+                type: String
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
