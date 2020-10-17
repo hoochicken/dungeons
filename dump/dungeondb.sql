b8b735b0694e9ad3feb4e85d4aacfb4f8c84254d
hoochicken <mareike.riegel@ql.de>
2020-10-17 16:07:55 +0200

    dump renewed

--- dump/dungeondb.sql
+++ dump/dungeondb.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: dungeon_mysql
--- Erstellungszeit: 02. Okt 2020 um 17:34
+-- Erstellungszeit: 17. Okt 2020 um 14:07
 -- Server-Version: 8.0.21
 -- PHP-Version: 7.4.6
 
@@ -137,7 +137,7 @@ CREATE TABLE `hero` (
   `type` int NOT NULL,
   `class` int NOT NULL,
   `description` varchar(255) NOT NULL,
-  `pic` varchar(255) NOT NULL,
+  `pic` varchar(2000) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   `le` int NOT NULL,
   `le_current` int NOT NULL,
   `ae` int NOT NULL DEFAULT '0',
@@ -262,6 +262,8 @@ CREATE TABLE `place` (
   `id` int NOT NULL,
   `name` varchar(255) NOT NULL,
   `description` varchar(1023) NOT NULL,
+  `pic` varchar(2000) NOT NULL,
+  `misc` varchar(500) NOT NULL,
   `type` int DEFAULT '0',
   `attributes` varchar(1023) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL DEFAULT '{}',
   `state` tinyint(1) NOT NULL DEFAULT '1',
@@ -277,13 +279,57 @@ CREATE TABLE `place` (
 -- Daten für Tabelle `place`
 --
 
-INSERT INTO `place` (`id`, `name`, `description`, `type`, `attributes`, `state`, `created`, `created_user`, `updated`, `updated_user`, `deleted`, `deleted_user`) VALUES
-(1, 'Wald', 'This ist the forest description, yess!', 1, '{}', 0, '2020-08-27 17:20:06', 0, NULL, 0, NULL, 0),
-(2, 'Sanddüne', '', 1, '{}', 1, '2020-08-27 17:20:06', 0, NULL, 0, NULL, 0),
-(3, 'Steinkreis', '', 1, '{}', 1, '2020-08-27 17:20:06', 0, NULL, 0, NULL, 0),
-(4, 'Hütte des Hellsehers', '', 1, '{}', 1, '2020-08-27 17:20:06', 0, NULL, 0, NULL, 0),
-(5, 'Berghöhle', '', 1, '{}', 1, '2020-08-27 17:20:06', 0, NULL, 0, NULL, 0),
-(6, 'Einhornwiese', '', 1, '{}', 1, '2020-08-27 17:20:06', 0, NULL, 0, NULL, 0);
+INSERT INTO `place` (`id`, `name`, `description`, `pic`, `misc`, `type`, `attributes`, `state`, `created`, `created_user`, `updated`, `updated_user`, `deleted`, `deleted_user`) VALUES
+(1, 'Waldeisens', 'This ist the forest description, yess!', 'C:\\fakepath\\download.png', 'NeOg8iCFfTA', 1, '{}', 0, '2020-08-27 17:20:06', 0, '2020-10-15 20:21:34', 0, NULL, 0),
+(2, 'Sanddüne', '', '', '', 1, '{}', 1, '2020-08-27 17:20:06', 0, NULL, 0, NULL, 0),
+(3, 'Steinkreis', '', '', '', 1, '{}', 1, '2020-08-27 17:20:06', 0, '2020-10-10 18:25:24', 0, NULL, 0),
+(4, 'Hütte des Hellsehers', '', '', '', 1, '{}', 1, '2020-08-27 17:20:06', 0, NULL, 0, NULL, 0),
+(5, 'Berghöhle', '', '', '', 1, '{}', 1, '2020-08-27 17:20:06', 0, NULL, 0, NULL, 0),
+(6, 'Einhornwiese', '', '', '', 1, '{}', 1, '2020-08-27 17:20:06', 0, NULL, 0, NULL, 0),
+(13, 'A new Place', 'Holla die Waldfee', '', '', 0, '{}', 1, '2020-10-16 21:28:01', 0, NULL, 0, NULL, 0),
+(14, 'A new Place', 'Holla die Waldfee', '', '', 0, '{}', 1, '2020-10-16 21:30:16', 0, NULL, 0, NULL, 0),
+(15, 'A new Place', 'Holla die Waldfee', '', '', 0, '{}', 1, '2020-10-16 21:31:17', 0, NULL, 0, NULL, 0),
+(16, 'A new Place', 'Holla die Waldfee', '', '', 0, '{}', 1, '2020-10-16 21:31:35', 0, NULL, 0, NULL, 0),
+(17, 'A new Place', 'Holla die Waldfee', '', '', 0, '{}', 1, '2020-10-16 21:31:47', 0, NULL, 0, NULL, 0),
+(18, 'A new Place', 'Holla die Waldfee', '', '', 0, '{}', 1, '2020-10-16 21:33:13', 0, NULL, 0, NULL, 0),
+(19, 'aasdasd', 'Holla die Waldfee', '', '', 0, '{}', 1, '2020-10-17 09:17:57', 0, NULL, 0, NULL, 0),
+(20, 'aasdasd', 'Holla die Waldfee', '', '', 0, '{}', 1, '2020-10-17 09:20:40', 0, NULL, 0, NULL, 0),
+(21, 'aasdasd', 'Holla die Waldfee', '', '', 0, '{}', 1, '2020-10-17 09:30:40', 0, NULL, 0, NULL, 0),
+(22, 'aasdasd', 'Holla die Waldfee', '', '', 0, '{}', 1, '2020-10-17 09:47:32', 0, NULL, 0, NULL, 0),
+(23, 'aasdasd', 'Holla die Waldfee', '', '', 0, '{}', 1, '2020-10-17 09:48:54', 0, NULL, 0, NULL, 0),
+(24, 'aasdasd', 'Holla die Waldfee', '', '', 0, '{}', 1, '2020-10-17 09:51:39', 0, NULL, 0, NULL, 0),
+(25, 'aasdasd', 'Holla die Waldfee', '', '', 0, '{}', 1, '2020-10-17 09:55:33', 0, NULL, 0, NULL, 0),
+(26, 'aasdasd', 'Holla die Waldfee', '', '', 0, '{}', 1, '2020-10-17 09:56:01', 0, NULL, 0, NULL, 0),
+(27, 'aasdasd', 'Holla die Waldfee', '', '', 0, '{}', 1, '2020-10-17 09:57:41', 0, NULL, 0, NULL, 0),
+(28, 'New Place', '', '', '', 0, '{}', 0, '2020-10-17 10:43:20', 0, NULL, 0, NULL, 0),
+(29, 'New Place', '', '', '', 0, '{}', 1, '2020-10-17 10:46:28', 0, NULL, 0, NULL, 0),
+(30, 'New Place', '', '', '', 0, '{}', 1, '2020-10-17 10:50:23', 0, NULL, 0, NULL, 0),
+(31, 'New Place', '', '', '', 0, '{}', 1, '2020-10-17 10:51:37', 0, NULL, 0, NULL, 0),
+(32, 'New Place', '', '', '', 0, '{}', 1, '2020-10-17 10:51:39', 0, NULL, 0, NULL, 0),
+(33, 'New Place', '', '', '', 0, '{}', 1, '2020-10-17 10:52:44', 0, NULL, 0, NULL, 0),
+(34, 'New Place', '', '', '', 0, '{}', 1, '2020-10-17 10:54:53', 0, NULL, 0, NULL, 0),
+(35, 'New Place', '', '', '', 0, '{}', 1, '2020-10-17 11:13:24', 0, NULL, 0, NULL, 0),
+(36, 'New Place', '', '', '', 0, '{}', 1, '2020-10-17 11:27:08', 0, NULL, 0, NULL, 0),
+(37, 'New Place', '', '', '', 0, '{}', 1, '2020-10-17 11:28:07', 0, NULL, 0, NULL, 0),
+(38, 'New Place', '', '', '', 0, '{}', 1, '2020-10-17 12:51:32', 0, NULL, 0, NULL, 0),
+(39, 'New Place', '', '', '', 0, '{}', 1, '2020-10-17 12:52:37', 0, NULL, 0, NULL, 0),
+(40, 'New Place', '', '', '', 0, '{}', 1, '2020-10-17 12:53:12', 0, NULL, 0, NULL, 0),
+(41, 'New Place', '', '', '', 0, '{}', 1, '2020-10-17 12:54:08', 0, NULL, 0, NULL, 0),
+(42, 'New Place', '', '', '', 0, '{}', 1, '2020-10-17 12:59:26', 0, NULL, 0, NULL, 0),
+(43, 'Unicorn Meadow', 'You are standing on a wonderful green meadow, with flowers blooming and butterflies swarming about.', '', '', 0, '{}', 1, '2020-10-17 13:00:56', 0, '2020-10-17 13:03:52', 0, NULL, 0),
+(44, 'Rotten Egg', 'Lorem Ipsum', '', '', 0, '{}', 1, '2020-10-17 13:04:02', 0, '2020-10-17 13:04:29', 0, NULL, 0),
+(45, 'New Place', '', '', '', 0, '{}', 1, '2020-10-17 13:11:07', 0, NULL, 0, NULL, 0),
+(46, 'New Place', '', '', '', 0, '{}', 1, '2020-10-17 13:11:49', 0, NULL, 0, NULL, 0),
+(47, 'The witch house at the brim of the forest', 'In the shade of a wellknown tree is seated a small hut. Smoke is evading the chimney.', '', '', 0, '{}', 1, '2020-10-17 13:12:41', 0, '2020-10-17 13:14:08', 0, NULL, 0),
+(48, 'Yes!', 'Lorem Ipsum II', '', '', 0, '{}', 1, '2020-10-17 13:14:47', 0, '2020-10-17 13:15:13', 0, NULL, 0),
+(49, 'Whatever', '', '', '', 0, '{}', 1, '2020-10-17 13:25:31', 0, '2020-10-17 13:26:18', 0, NULL, 0),
+(50, 'New Place', '', '', '', 0, '{}', 1, '2020-10-17 13:35:44', 0, NULL, 0, NULL, 0),
+(51, 'New Place', '', '', '', 0, '{}', 1, '2020-10-17 13:36:25', 0, NULL, 0, NULL, 0),
+(52, 'New Place', '', '', '', 0, '{}', 1, '2020-10-17 13:37:44', 0, NULL, 0, NULL, 0),
+(53, 'New Place', '', '', '', 0, '{}', 1, '2020-10-17 13:38:39', 0, NULL, 0, NULL, 0),
+(54, 'New Place', '', '', '', 0, '{}', 1, '2020-10-17 13:39:06', 0, NULL, 0, NULL, 0),
+(55, 'New Place', '', '', '', 0, '{}', 1, '2020-10-17 13:39:10', 0, NULL, 0, NULL, 0),
+(56, 'New Place', '', '', '', 0, '{}', 1, '2020-10-17 13:39:14', 0, NULL, 0, NULL, 0);
 
 -- --------------------------------------------------------
 
@@ -293,8 +339,8 @@ INSERT INTO `place` (`id`, `name`, `description`, `type`, `attributes`, `state`,
 
 CREATE TABLE `route` (
   `id` int NOT NULL,
-  `out` int NOT NULL,
-  `in` int NOT NULL,
+  `place_out` int NOT NULL,
+  `place_in` int NOT NULL,
   `out_direction` int NOT NULL,
   `attributes` varchar(1023) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL DEFAULT '{}',
   `state` tinyint(1) NOT NULL DEFAULT '1',
@@ -310,15 +356,15 @@ CREATE TABLE `route` (
 -- Daten für Tabelle `route`
 --
 
-INSERT INTO `route` (`id`, `out`, `in`, `out_direction`, `attributes`, `state`, `created`, `created_user`, `updated`, `updated_user`, `deleted`, `deleted_user`) VALUES
-(1, 0, 1, 2, '{}', 1, '2020-08-27 17:20:06', 0, NULL, 0, NULL, 0),
-(3, 0, 3, 2, '{}', 1, '2020-08-27 17:20:06', 0, NULL, 0, NULL, 0),
-(4, 0, 2, 1, '{}', 1, '2020-08-27 17:20:06', 0, NULL, 0, NULL, 0),
-(5, 0, 2, 3, '{}', 1, '2020-08-27 17:20:06', 0, NULL, 0, NULL, 0),
-(7, 0, 3, 4, '{}', 1, '2020-08-27 17:20:06', 0, NULL, 0, NULL, 0),
-(8, 0, 4, 5, '{}', 1, '2020-08-27 17:20:06', 0, NULL, 0, NULL, 0),
-(9, 0, 5, 6, '{}', 1, '2020-08-27 17:20:06', 0, NULL, 0, NULL, 0),
-(10, 0, 6, 5, '{}', 1, '2020-08-27 17:20:06', 0, NULL, 0, NULL, 0);
+INSERT INTO `route` (`id`, `place_out`, `place_in`, `out_direction`, `attributes`, `state`, `created`, `created_user`, `updated`, `updated_user`, `deleted`, `deleted_user`) VALUES
+(1, 2, 1, 2, '{}', 1, '2020-08-27 17:20:06', 0, NULL, 0, NULL, 0),
+(3, 2, 3, 2, '{}', 1, '2020-08-27 17:20:06', 0, NULL, 0, NULL, 0),
+(4, 1, 2, 1, '{}', 1, '2020-08-27 17:20:06', 0, NULL, 0, NULL, 0),
+(5, 4, 2, 3, '{}', 1, '2020-08-27 17:20:06', 0, NULL, 0, NULL, 0),
+(8, 6, 4, 5, '{}', 1, '2020-08-27 17:20:06', 0, NULL, 0, NULL, 0),
+(9, 6, 5, 6, '{}', 1, '2020-08-27 17:20:06', 0, NULL, 0, NULL, 0),
+(10, 4, 6, 5, '{}', 1, '2020-08-27 17:20:06', 0, NULL, 0, NULL, 0),
+(21, 4, 4, 6, '{}', 1, '2020-10-17 11:26:46', 0, NULL, 0, NULL, 0);
 
 --
 -- Indizes der exportierten Tabellen
@@ -383,7 +429,7 @@ ALTER TABLE `place`
 --
 ALTER TABLE `route`
   ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `in` (`in`,`out_direction`);
+  ADD UNIQUE KEY `in` (`place_in`,`out_direction`);
 
 --
 -- AUTO_INCREMENT für exportierte Tabellen
@@ -423,13 +469,13 @@ ALTER TABLE `item`
 -- AUTO_INCREMENT für Tabelle `place`
 --
 ALTER TABLE `place`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=57;
 
 --
 -- AUTO_INCREMENT für Tabelle `route`
 --
 ALTER TABLE `route`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=40;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
