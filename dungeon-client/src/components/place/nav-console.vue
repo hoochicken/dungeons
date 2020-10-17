ff7e100301a2c92ba710d39a0d14832250289b39
hoochicken <mareike.riegel@ql.de>
2020-10-17 10:34:46 +0200

    nav-console now has general useRoute() method

--- dungeon-client/src/components/place/nav-console.vue
+++ dungeon-client/src/components/place/nav-console.vue
@@ -3,42 +3,20 @@
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
         <div class="md-layout-item md-layout md-gutter">
             <div @click="useRoute(1)" class="btn btn1 md-layout-item md-size-10"><md-icon class="fa fa-arrow-up"></md-icon></div>
-            <div></div>
             <div @click="useRoute(2)" class="btn btn2 md-layout-item"><md-icon class="fa fa-arrow-up"></md-icon></div>
-            <div></div>
             <div @click="useRoute(3)" class="btn btn3 md-layout-item md-size-10"><md-icon class="fa fa-arrow-up"></md-icon></div>
         </div>
         <div class="md-layout-item md-layout md-gutter">
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
-        </div>
-
-        <div class="md-layout-item md-layout md-gutter">
             <div @click="useRoute(4)" class="btn btn4 md-layout-item md-size-10"><md-icon class="fa fa-arrow-left"></md-icon></div>
-            <div></div>
             <div class="btn content md-layout-item">
                 CENTRE
             </div>
-            <div></div>
             <div @click="useRoute(5)" class="btn btn5 md-layout-item md-size-10"><md-icon class="fa fa-arrow-right"></md-icon></div>
         </div>
 
         <div class="md-layout-item md-layout md-gutter">
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
-        </div>
-
-        <div class="md-layout-item md-layout md-gutter">
             <div @click="useRoute(6)" class="btn btn6 md-layout-item md-size-10"><md-icon class="fa fa-arrow-down"></md-icon></div>
-            <div></div>
             <div @click="useRoute(7)" class="btn btn7 md-layout-item"><md-icon class="fa fa-arrow-down"></md-icon></div>
-            <div></div>
             <div @click="useRoute(8)" class="btn btn8 md-layout-item md-size-10"><md-icon class="fa fa-arrow-down"></md-icon></div>
         </div>
     </div>
@@ -54,25 +32,7 @@
         },
         methods: {
             async useRoute(direction) {
-                try {
-                    this.error = {};
-
-                    // create place
-                    let newPlaceId = this.initiatePlace();
-                    let asd = false;
-                    if (asd) {
-
-                    // create route
-                    this.createRoute(this.placeId, newPlaceId, direction);
-
-                    // move to place
-                    this.$route.push('/place/display/' + newPlaceId);
-
-                    return this.response.data.id;
-                    }
-                } catch (error) {
-                    this.error = error.response;
-                }
+                this.createNewRoute(direction);
             },
             async createNewRoute(direction) {
                 try {
@@ -130,7 +90,7 @@
 <style scoped>
     .content {}
     .md-layout-item.md-layout.md-gutter {}
-    .md-layout-item.md-layout.md-gutter > div {padding:0;width: 20%;height: 20px;text-align: center;}
+    .md-layout-item.md-layout.md-gutter > div {padding:0;width: 33px;height: 33px;text-align: center;}
     .btn {font-size:20px;cursor:pointer;display:block; padding:0;text-align: center;}
     .btn1 {transform: rotate(-45deg);}
     .btn2 {}
