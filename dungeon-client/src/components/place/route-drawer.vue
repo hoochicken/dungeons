d20223ab1b9106aec64f5a42320ba4c733d64d25
hoochicken <mareike.riegel@ql.de>
2020-10-18 12:41:38 +0200

    palce update route drawer: places are now displayed in select box

--- dungeon-client/src/components/place/route-drawer.vue
+++ dungeon-client/src/components/place/route-drawer.vue
@@ -2,25 +2,29 @@
     <div>
         <md-toolbar class="md-transparent" md-elevation="0">
             <span class="md-title">Route</span>
-            <div class="content">
-                <md-button v-if="0 === routeId" class="md-raised md-primary" @click="createRoute">Create</md-button>
-                <md-button v-else class="md-raised md-primary" @click="updateRoute(placeIn)">Walk To ({{ placeIn }})</md-button>
-                <md-button v-if="0 < routeId" class="md-raised md-accent" @click="deleteRoute">Delete</md-button>
-                <md-button class="md-raised" @click="$emit('closeDrawer');">Cancel</md-button>
+        </md-toolbar>
+        <div class="content">
+            <md-button v-if="0 === routeId" class="md-raised md-primary" @click="createRoute">Create</md-button>
+            <md-button v-else class="md-raised md-primary" @click="updateRoute(placeIn)">Walk To ({{ placeIn }})</md-button>
+            <md-button v-if="0 < routeId" class="md-raised md-accent" @click="deleteRoute">Delete</md-button>
+            <md-button class="md-raised" @click="$emit('closeDrawer');">Cancel</md-button>
 
 
-                <div v-if="0 < routeId">
-                    {{ places }}
-                    <!--md-select v-model="placeIn">
-                        <md-option v-for="item in places" :value="item.id" :key="item.id">{{ item.name }}</md-option>
-                    </md-select-->
-                </div>
+            <div v-if="0 < routeId">
+                {{ newPlace }}
+                <md-field>
+                    <label for="newPlace">Place</label>
+                    <md-select v-model="newPlace" name="newPlace" id="newPlace">
+                        <md-option v-for="item in places" :value="item.id" :key="item.id">{{ item.name }} ({{ item.id }})</md-option>
+                    </md-select>
+                </md-field>
+
+                {{ places }}
+            </div>
             {{ placeId }}
             {{ outDirection }}
             {{ routeId }}
-            </div>
-
-        </md-toolbar>
+        </div>
     </div>
 </template>
 
@@ -50,7 +54,7 @@
                     totalPage: 0,
                     totalItems: 0
                 },
-                placeUpdated: this.placeIn
+                newPlace: this.placeIn
             }
         },
         async mounted() {
