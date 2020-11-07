fad87ddb08bcc6c34474422e6be4bcabc09c299b
hoochicken <mareike.riegel@ql.de>
2020-11-07 20:47:04 +0100

    direction-select generated

---
+++ dungeon-client/src/components/global/direction-select.vue
@@ -0,0 +1,100 @@
+<template>
+    <div class="md-field">
+        <select id="directionNew" required v-model="directionNew">
+            <option v-for="item in directions" :key="item.out_direction" :selected="directionNew == item.out_direction"
+                    :value="item.out_direction">{{ item.label }} ({{ item.out_direction }})
+                <md-icon v-if="item.place_in > 0" class="fa fa-arrow-up md-primary"></md-icon>
+                <md-icon v-else class="fa fa-arrow-up"></md-icon>
+            </option>
+        </select>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "direction-select",
+        props: {
+            edit: {
+                type: Boolean,
+                default: false
+            },
+            routeId: {
+                type: Number,
+                default: 0
+            },
+            direction: {
+                type: Number,
+                default: 0
+            }
+        },
+        data() {
+            return {
+                directionNew: 0,
+                directions: {}
+            }
+        },
+        async mounted() {
+            this.directionNew = this.direction;
+            await this.getDirections();
+        },
+        watch: {
+            direction: async function () {
+                this.directionNew = this.direction;
+                await this.getDirections();
+            },
+            directionNew: async function () {
+                this.$emit('setDirection', this.directionNew);
+            }
+        },
+        methods: {
+            async getDirections() {
+                console.log('getDirections');
+                try {
+                    let response = await this.axios.post('/route/directions');
+                    this.directions = response.data.items;
+                } catch (error) {
+                    this.error = error.response;
+                }
+            }
+        }
+
+    }
+</script>
+<style scoped>
+
+    .btn1 {
+        transform: rotate(-45deg);
+    }
+
+    .btn2 {
+    }
+
+    .btn3 {
+        transform: rotate(45deg);
+    }
+
+    .btn4 {
+        transform: rotate(-90deg);
+    }
+
+    .btn5 {
+        opacity: 0;
+    }
+
+    .btn6 {
+        transform: rotate(90deg);
+    }
+
+    .btn7 {
+        transform: rotate(-135deg);
+    }
+
+    .btn8 {
+        transform: rotate(180deg);
+    }
+
+    .btn9 {
+        transform: rotate(135deg);
+    }
+
+</style>
