c939ebbd2928a31711ea8d5b1a4f396cea75d3a6
hoochicken <mareike.riegel@ql.de>
2020-12-19 10:46:49 +0100

    Item: vue files initialized, some weird bug included

---
+++ dungeon-client/src/components/item/display.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h1>Item - List</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "display"
+    }
+</script>
+
+<style scoped>
+
+</style>
