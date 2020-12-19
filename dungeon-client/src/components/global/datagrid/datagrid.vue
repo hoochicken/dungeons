110948b2c4f81caff83499b0a1d5d2946a4c5dd9
hoochicken <mareike.riegel@ql.de>
2020-12-19 11:38:21 +0100

    Datagrid: header useless if stripped

--- dungeon-client/src/components/global/datagrid/datagrid.vue
+++ dungeon-client/src/components/global/datagrid/datagrid.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <md-table>
-            <thead v-if="header !== undefined && header.length > 0">
+            <thead v-if="header.length > 0">
                 <md-table-row>
                     <md-table-cell v-for="item in header" v-bind:key="item.id">{{ item }}</md-table-cell>
                 </md-table-row>
