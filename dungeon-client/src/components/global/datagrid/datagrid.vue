ac3412fb838f1a1dbde4cc12d25b76142c2f0008
hoochicken <mareike.riegel@ql.de>
2020-12-19 11:18:10 +0100

    Datagrid: initialized and implemented in item list

---
+++ dungeon-client/src/components/global/datagrid/datagrid.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <table>
+            <thead v-if="header.length > 0">
+                <md-table-row>
+                    <md-table-cell v-for="item = header">{{ item }}</md-table-cell>
+                </md-table-row>
+            </thead>
+        </table>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "datagrid",
+        props: {
+            data: {
+                type: Array,
+                default: []
+            },
+            header: {
+                type: Array,
+                default: []
+            },
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
--- dungeon-client/src/components/item/list.vue
+++ dungeon-client/src/components/item/list.vue
@@ -1,12 +1,34 @@
 <template>
     <div>
         <h1>Item - List</h1>
+        {{ header }}
+        <datagrid :data="data" :header="header"></datagrid>
     </div>
 </template>
 
 <script>
+    import Datagrid from "../global/datagrid/datagrid";
+
     export default {
-        name: "list"
+        name: "list",
+        components: {Datagrid},
+        mounted() {
+            this.setHeader();
+            this.loadList();
+        },
+        methods: {
+            async loadList() {
+                this.data = [
+                    {id: 2, title: 'asdtitle', column: 'asdad', column2: 'sadadasdd'},
+                    {id: 3, title: 'asd d a title', column: 'asdad', column2: 'sadadasdd'},
+                    {id: 5, title: 'asdtiasd a sdf tle', column: 'asdad', column2: 'sadadasdd'},
+                    {id: 13, title: 'as sf dtitle', column: 'asdad', column2: 'sadadasdd'},
+                ];
+            },
+            setHeader() {
+                this.header = [ 'Id', 'asdasd', '',  'what' ];
+            }
+        }
     }
 </script>
 
