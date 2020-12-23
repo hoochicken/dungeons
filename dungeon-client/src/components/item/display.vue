c54d80801fec9cb6734d3c796668038632d847ec
hoochicken <mareike.riegel@ql.de>
2020-12-23 07:50:35 +0100

    Item > Display: getItem() works now, yrss

--- dungeon-client/src/components/item/display.vue
+++ dungeon-client/src/components/item/display.vue
@@ -1,18 +1,39 @@
 <template>
     <div>
         <h1>Item - Display</h1>
+        {{ item }}
     </div>
 </template>
 
 <script>
     export default {
         name: "display",
+        data() {
+            return {
+                loading: false,
+                itemId: 0,
+                response: {},
+                item: {},
+                displayError: false,
+            }
+        },
         mounted() {
-          this.loadItem();
+          this.loadItem(this.$route.params.id);
         },
         methods: {
-            loadItem() {
-
+            async loadItem(id) {
+                try {
+                    this.loading = true;
+                    this.itemId = parseInt(id);
+                    this.response = await this.axios.get('/item/get/' + id);
+                    this.item = this.response.data;
+                    this.displayError = false;
+                    this.loading = false;
+                } catch (error) {
+                    this.error = error;
+                    this.displayError = true;
+                    this.loading = true;
+                }
             }
         }
     }
