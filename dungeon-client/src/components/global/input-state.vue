190533571c997ea9fd2d14592f137895cf586b1e
hoochicken <mareike.riegel@ql.de>
2020-12-28 20:39:54 +0100

    COmponent input-state implemented

---
+++ dungeon-client/src/components/global/input-state.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>{{ value }}
+        <select v-bind:value="value" v-on:input="$emit('input', $event.target.value)">
+            <option value="1">Published</option>
+            <option value="0">Njet!</option>
+        </select>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "input-state",
+        props: {
+            value: {
+                type: Number,
+                default: 0
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
