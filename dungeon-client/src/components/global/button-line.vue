cd8f8df318d79a9d010e980319c12d1d554042d6
hoochicken <mareike.riegel@ql.de>
2020-12-31 14:22:38 +0100

    Button line: delete confirm() added

--- dungeon-client/src/components/global/button-line.vue
+++ dungeon-client/src/components/global/button-line.vue
@@ -1,6 +1,6 @@
 <template>
     <div :class="'d-flex mb-3 ' + (itemId > 0 ? 'justify-content-end' : 'justify-content-end')">
-        <md-button v-if="itemId > 0" class="md-raised md-accent" @click="$emit('delete', itemId)">Delete</md-button>
+        <md-button v-if="itemId > 0" class="md-raised md-accent" @click="deleteEntry">Delete</md-button>
         <md-button v-if="itemId > 0" class="md-raised md-primary" @click="$emit('update', itemId)">Update</md-button>
         <md-button v-if="itemId > 0" class="md-raised md-primary" @click="$emit('display', itemId)">Display</md-button>
         <md-button v-else class="md-raised md-primary" @click="$emit('create')">Create</md-button>
@@ -15,6 +15,12 @@
             itemId: {
                 type: Number,
                 default: 0
+            }
+        },
+        methods: {
+            deleteEntry() {
+                if (!confirm('Really delete this entry with ' + this.itemId)) return;
+                this.$emit('delete', this.itemId);
             }
         }
     }
