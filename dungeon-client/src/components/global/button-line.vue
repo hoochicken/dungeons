ba371276db6c8f7c48379e3a6a33556a75f1745c
hoochicken <mareike.riegel@ql.de>
2020-12-31 13:50:16 +0100

    Button line now has display

--- dungeon-client/src/components/global/button-line.vue
+++ dungeon-client/src/components/global/button-line.vue
@@ -1,9 +1,10 @@
 <template>
     <div :class="'d-flex mb-3 ' + (itemId > 0 ? 'justify-content-end' : 'justify-content-end')">
         <md-button v-if="itemId > 0" class="md-raised md-accent" @click="$emit('delete', itemId)">Delete</md-button>
-        <md-button class="md-raised" @click="$emit('cancel')">Cancel</md-button>
         <md-button v-if="itemId > 0" class="md-raised md-primary" @click="$emit('update', itemId)">Update</md-button>
+        <md-button v-if="itemId > 0" class="md-raised md-primary" @click="$emit('display', itemId)">Display</md-button>
         <md-button v-else class="md-raised md-primary" @click="$emit('create')">Create</md-button>
+        <md-button class="md-raised" @click="$emit('cancel')">Cancel</md-button>
     </div>
 </template>
 
